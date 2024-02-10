73fd0c44237508bc6fe5ec2291d226fee8ba81ad
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-02-10 17:30:39 +0900

    added About bgImage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  webpack(config) {
+    // Add the SVG loader configuration
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
