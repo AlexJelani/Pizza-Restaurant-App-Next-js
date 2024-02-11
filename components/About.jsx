65b8ac84a2bc78dc025135646141eeb11b51ac9c
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-02-11 14:22:22 +0900

    added about image and home image

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import place from '../public/img/restaurant.jpg';
+import place from '../public/img/takipizzabout.png';
 
 function About() {
 
